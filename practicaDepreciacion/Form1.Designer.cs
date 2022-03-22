d08c0ca1234e7ef2418d4a6001f7ab71b1444c39
yaderxd <yaderoviedo4@gmail.com>
2022-03-21 22:48:20 -0600

    Agregar archivos de proyecto.

---
+++ practicaDepreciacion/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace practicaDepreciacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtValorR = new System.Windows.Forms.TextBox();
+            this.txtVidaU = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtIdBusca = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Image = ((System.Drawing.Image)(resources.GetObject("btnEnviar.Image")));
+            this.btnEnviar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnEnviar.Location = new System.Drawing.Point(80, 350);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(116, 40);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "       Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.txtEnviar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(97, 18);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(97, 81);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 2;
+            this.txtValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValor_KeyPress_1);
+            // 
+            // txtValorR
+            // 
+            this.txtValorR.Location = new System.Drawing.Point(97, 149);
+            this.txtValorR.Name = "txtValorR";
+            this.txtValorR.Size = new System.Drawing.Size(100, 20);
+            this.txtValorR.TabIndex = 3;
+            this.txtValorR.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValorR_KeyPress);
+            // 
+            // txtVidaU
+            // 
+            this.txtVidaU.Location = new System.Drawing.Point(97, 219);
+            this.txtVidaU.Name = "txtVidaU";
+            this.txtVidaU.Size = new System.Drawing.Size(100, 20);
+            this.txtVidaU.TabIndex = 4;
+            this.txtVidaU.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVidaU_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Valor residual:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Vida util:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(378, 238);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(447, 152);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Image = ((System.Drawing.Image)(resources.GetObject("btnActualizar.Image")));
+            this.btnActualizar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnActualizar.Location = new System.Drawing.Point(155, 29);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(116, 40);
+            this.btnActualizar.TabIndex = 10;
+            this.btnActualizar.Text = "      Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Image = ((System.Drawing.Image)(resources.GetObject("btnBorrar.Image")));
+            this.btnBorrar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnBorrar.Location = new System.Drawing.Point(300, 29);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(116, 40);
+            this.btnBorrar.TabIndex = 11;
+            this.btnBorrar.Text = "     Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel1.Controls.Add(this.txtValorR);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.txtValor);
+            this.panel1.Controls.Add(this.txtVidaU);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(35, 46);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(221, 266);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtIdBusca);
+            this.panel2.Controls.Add(this.btnBorrar);
+            this.panel2.Controls.Add(this.btnActualizar);
+            this.panel2.Location = new System.Drawing.Point(378, 104);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(429, 90);
+            this.panel2.TabIndex = 15;
+            // 
+            // txtIdBusca
+            // 
+            this.txtIdBusca.Location = new System.Drawing.Point(15, 49);
+            this.txtIdBusca.Name = "txtIdBusca";
+            this.txtIdBusca.Size = new System.Drawing.Size(104, 20);
+            this.txtIdBusca.TabIndex = 12;
+            this.txtIdBusca.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIdBusca_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(53, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(894, 460);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEnviar);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Activo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtValorR;
+        private System.Windows.Forms.TextBox txtVidaU;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtIdBusca;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ practicaDepreciacion/Form1.cs
@@ -0,0 +1,269 @@
+﻿using AppCore.IServices;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicaDepreciacion
+{
+    public partial class Form1 : Form
+    {
+        IActivoServices activoServices;
+        public Form1(IActivoServices ActivoServices)
+        {
+            this.activoServices = ActivoServices;
+            InitializeComponent();
+        }
+
+
+        #region barras de texto
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede numeros");
+            }
+        }
+
+        private void txtValor_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede LETRAS");
+            }
+        }
+        private void txtIdbuscar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede LETRAS");
+            }
+        }
+        private void toolStripTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede LETRAS");
+            }
+        }
+
+        private void txtValorR_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede LETRAS");
+            }
+        }
+
+        private void txtVidaU_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede LETRAS");
+            }
+        }
+
+        private void txtIdBusca_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No se puede LETRAS");
+            }
+        }
+        private bool verificar()
+        {
+            if (String.IsNullOrEmpty(txtNombre.Text) || String.IsNullOrEmpty(txtValor.Text) || String.IsNullOrEmpty(txtVidaU.Text) || String.IsNullOrEmpty(txtValorR.Text))
+            {
+
+                return false;
+            }
+            return true;
+        }
+        private void limpiar()
+        {
+            this.txtNombre.Text = String.Empty;
+            this.txtValor.Text = String.Empty;
+            this.txtValorR.Text = String.Empty;
+            this.txtVidaU.Text = String.Empty;
+        }
+        #endregion
+
+
+
+
+        private void txtEnviar_Click(object sender, EventArgs e)
+        {
+            bool verificado = verificar();
+            if (verificado == false)
+            {
+                MessageBox.Show("Tienes que llenar todos los formularios.");
+            }
+            else
+            {
+
+                Activo activo = new Activo()
+                {
+                    Nombre = txtNombre.Text,
+                    Valor = double.Parse(txtValor.Text),
+                    ValorResidual=double.Parse(txtValorR.Text),
+                    VidaUtil= int.Parse(txtVidaU.Text)
+                };
+                activoServices.Add(activo);
+                dataGridView1.DataSource = null;
+                limpiar();
+                dataGridView1.DataSource = activoServices.Read();
+
+            }
+        }
+       
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                FrmDepreciacion depreciacion = new FrmDepreciacion(activoServices.Read()[e.RowIndex]);
+                depreciacion.ShowDialog();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = activoServices.Read();
+        
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Aseguresé de escribir el Id");
+
+            DialogResult a = MessageBox.Show("¿Esta seguro de actualizar el archivo?", "Actualizando el archivo...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (a == DialogResult.Yes)
+            {
+                bool verificado = verificar();
+                if (verificado == false)
+                {
+                    MessageBox.Show("Tienes que llenar todos los formularios.");
+                }
+                else
+                {
+
+                    Activo activo = new Activo()
+                    {
+                        Nombre = txtNombre.Text,
+                        Valor = double.Parse(txtValor.Text),
+                        ValorResidual = double.Parse(txtValorR.Text),
+                        VidaUtil = int.Parse(txtVidaU.Text)
+                    };
+                    activoServices.Update(activo, int.Parse(txtIdBusca.Text));
+                    dataGridView1.DataSource = null;
+                    limpiar();
+                    dataGridView1.DataSource = activoServices.Read();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Se cancelo esta acción");
+            }
+
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+
+
+
+
+
+            DialogResult a = MessageBox.Show("¿Esta seguro de borrar el archivo?", "Borrando dato...", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (a == DialogResult.Yes)
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("Se cancelo esta acción");
+            }
+
+
+
+        }
+
+        
+
+
+
+        //private void crearToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    btnEnviar.Show(); txtNombre.Show();
+        //    txtValor.Show();
+        //    txtValorR.Show();
+        //    txtVidaU.Show();
+        //    label1.Show();
+        //    label2.Show();
+        //    label3.Show();
+        //    label4.Show();
+        //}
+        //btnActualizar.Hide();
+        //btnBorrar.Hide();
+        //DialogResult a = MessageBox.Show("¿Actualizar?", "¿Que esta solicitando?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        //           if (a == DialogResult.Yes)
+        //{
+        //    btnBorrar.Hide();
+        //    btnEnviar.Hide();
+        //    btnActualizar.Show();
+        //    txtNombre.Show();
+        //    txtValor.Show();
+        //    txtValorR.Show();
+        //    txtVidaU.Show();
+        //    label1.Show();
+        //    label2.Show();
+        //    label3.Show();
+        //    label4.Show();
+        //}
+        //else {
+        //    DialogResult b = MessageBox.Show("¿Borrar?", "¿Que esta solicitando?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+        //    if (b == DialogResult.Yes)
+        //    {
+        //        btnEnviar.Hide();
+        //        btnActualizar.Hide();
+        //        btnBorrar.Show();
+        //        txtNombre.Hide();
+        //        txtValor.Hide();
+        //        txtValorR.Hide();
+        //        txtVidaU.Hide();
+        //        label1.Hide();
+        //        label2.Hide();
+        //        label3.Hide();
+        //        label4.Hide();
+
+        //    }
+        //    else
+        //    {
+        //        btnEnviar.Show();
+        //        MessageBox.Show("No se seleccionó ninguna opción");
+        //    }
+
+        //}
+    }
+}
